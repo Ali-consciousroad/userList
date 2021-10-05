e51bab49f635dc673c73e610dea3dcb921b2471a
Ali-consciousroad <ali_dindar@live.be>
2021-10-05 15:35:58 +0200

    Working with State and Events

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,37 +1,83 @@
-import { useState } from 'react';
-import User from './User';
+import { Component } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+  constructor() {
+    super();
+    // Important: In class based component, state is always an object
+    /* In class bases components, states need to be regrouped in an object
+      and this is not optional even if they are not related */
+    this.state = {
+      showUsers: true,
+      more: "Test",
+    };
+  }
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+  toggleUsersHandler() {
+    // Not the way to update the state
+    // this.state.showUsers = false;
+    // Right way to update the state
+    // We pass an object so now we have to return an object
+    /* setState() doesn't overwrite the state but update it 
+      contratry to useState() used with functional comp. */
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {/* Access showUsers property from this.state object */}
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     // Update the state
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+// return (
+//   <div className={classes.users}>
+//     <button onClick={toggleUsersHandler}>
+//       {showUsers ? 'Hide' : 'Show'} Users
+//     </button>
+//     {showUsers && usersList}
+//   </div>
+// );
+// };
 
 export default Users;
